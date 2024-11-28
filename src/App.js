931b3dfd211a3ddb7f5a52dea6801bbe065e7628
harshitg927 <gandhiharshit716@gmail.com>
2024-11-28 12:32:25 +0530

    addition of login and registeration page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import EventCalendar from "./components/EventCalendar";
 import StudyGroups from "./components/StudyGroups";
 import LostAndFound from "./components/LostAndFound";
 import Announcements from "./components/Announcements";
+import LoginRegisterPage from "./components/LoginRegisterPage";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/" element={<Homepage />} />
           <Route path="/event-calendar" element={<EventCalendar />} />
           <Route path="/study-groups" element={<StudyGroups />} />
           <Route path="/lost-and-found" element={<LostAndFound />} />
-          <Route path="/announcements" element={<Announcements />} />
+          <Route path="/announcements" element={<Announcements />} /> */}
+          <Route path="/" element={<LoginRegisterPage />} />
         </Routes>
       </div>
     </Router>
